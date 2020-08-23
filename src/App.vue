270c6d3648b14840b26014e3d09c7a49eb143d38
morningglory1996 <acer080428@outlook.jp>
2020-08-23 15:03:16 +0900

    練習追加

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <LikeHeader header-text="HELLO">
+      <h2>デフォルトスロット</h2>
+
+      <template v-slot:title="slotProps">
+        <h1>タイトル</h1>
+        <h2>{{ slotProps.user.firstName }}</h2>
+      </template>
+
+      <template v-slot:number>
+        <h2>{{number}}</h2>
+      </template>
+
+      <template #[title2]>
+        <h1>TITLE</h1>
+      </template>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
+    <LikeNumber :total-number="number"></LikeNumber>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LikeHeader from "./components/LikeHeader";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      number: 10,
+      title2: "TITLE",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    LikeHeader,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+div {
+  border: 1px solid red;
 }
 </style>
